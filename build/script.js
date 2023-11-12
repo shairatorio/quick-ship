de3576e81159ce1c792ef318a3c0ac00391ad4c7
shairatorio <shairatorio08@gmail.com>
2023-11-12 21:38:59 +0800

    add validation in confirm password

---
+++ build/script.js
@@ -0,0 +1,10 @@
+function validatePassword() {
+    const password = document.querySelector('input[name=password]');
+    const confirm = document.querySelector('input[name=confirmPassword]');
+    if (confirm.value === password.value) {
+      confirm.setCustomValidity('');
+    } else {
+      confirm.setCustomValidity('Passwords do not match');
+    }
+  }
+
